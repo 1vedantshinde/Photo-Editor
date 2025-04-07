7e4cecb84cb927ba9eaeb069514af0beaafbd17f
v_shinde <139036307+1vedantshinde@users.noreply.github.com>
2025-04-07 08:55:09 +0530

    Update PhotoEditorMain.java

---
+++ src/PhotoEditorMain.java
@@ -0,0 +1,15 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.scene.image.Image;
+import photoeditor.filters.BlackAndWhiteFilter;
+
+public class PhotoEditorMain extends Application{
+
+
+  public static void main(String[] args){
+    launch();
+  }
+}
